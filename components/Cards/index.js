55886fa3b96dda7b431e39b7d975a3beb1f7ee6c
William M <wmooney1984@gmail.com>
2019-07-21 00:08:16 -0400

    MVP Complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,55 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+  .then(data => {
+    const axiosData = data.data.articles;
+    const cards_container = document.querySelector('.cards-container');
+
+    const topics = Object.keys(axiosData);
+
+    for(let i = 0; i < topics.length; i++) {
+      const topic = topics[i];
+
+      axiosData[topic].forEach((card)=> {
+        cards_container.appendChild(createCard(card));
+      });
+    }
+
+    function createCard(array) {
+      const headline = array.headline;
+      const photo = array.authorPhoto;
+      const name = array.authorName;
+
+      const card = document.createElement('div');
+      card.classList.add('card');
+
+      const headlineDiv = document.createElement('div');
+      headlineDiv.classList.add('headline');
+      headlineDiv.textContent = headline;
+      card.appendChild(headlineDiv);
+
+      const authorDiv =  document.createElement('div');
+      authorDiv.classList.add('author');
+      card.appendChild(authorDiv);
+
+      const imageDiv =  document.createElement('div');
+      imageDiv.classList.add('img-container');
+      authorDiv.appendChild(imageDiv);
+
+      const img = document.createElement('img');
+      img.src = photo;
+      imageDiv.appendChild(img);
+
+      const authorSpan = document.createElement('span');
+      authorSpan.textContent = `By ${name}`;
+      authorDiv.appendChild(authorSpan);
+
+      return card;
+    }
+  })
+
+  .catch(error => {
+    console.log("ERROR", error);
+  })
